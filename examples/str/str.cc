957da150d640b09e43829bd810ede9a052caec51
Xiaotian Pei <pxtian2008@gmail.com>
2015-05-25 09:40:07 -0700

    Add strtoint to base

--- examples/str/str.cc
+++ examples/str/str.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "base/stringprintf.h"
+#include "base/strtoint.h"
 #include "third_party/glog/logging.h"
 
 
@@ -8,5 +9,6 @@ int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   string greeting = StringPrintf("%s %s", "hello", "world");
   std::cout << greeting << std::endl;
+  std::cout << atoi64("1234567") << std::endl;
   return 0;
 }
