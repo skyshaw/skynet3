bb501f6254e8a3673b1f250a852f0e806443a1f4
Xiaotian Pei <pxtian2008@gmail.com>
2015-05-27 21:29:49 -0700

    Move re2 to third_party, and provide a wrapper for it.

--- examples/re2/regex.cc
+++ examples/re2/regex.cc
@@ -4,11 +4,11 @@
 #include <cassert>
 #include <cstdio>
 
-#include "re2/re2/re2.h"  // at least reduce one...
+#include "util/regexp/re2/re2.h"  // at least reduce one...
 
 int main() {
   int year, month, day;
-  RE2::FullMatch("1998 4 23", "(\\d+) (\\d+) (\\d+)", &year, &month, &day);
+  RE2::FullMatch("1998 5 24", "(\\d+) (\\d+) (\\d+)", &year, &month, &day);
   std::printf("%d %d %d\n", year, month, day);
   return 0;
 }
---
+++ util/regexp/re2/re2.h
@@ -0,0 +1,6 @@
+#ifndef UTIL_REGEXP_RE2_REGEXP_H__
+#define UTIL_REGEXP_RE2_REGEXP_H__
+
+#include "third_party/re2/re2/re2.h"
+
+#endif  // UTIL_REGEXP_RE2_REGEXP_H__
