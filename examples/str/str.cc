5fbf49015e8032c52b5b8e532e0dab3890860232
Xiaotian Pei <pxtian2008@gmail.com>
2015-05-27 20:23:55 -0700

    Add ascii_ctype and its example.

--- examples/str/str.cc
+++ examples/str/str.cc
@@ -1,8 +1,19 @@
 #include <iostream>
 #include "base/stringprintf.h"
 #include "base/strtoint.h"
+#include "strings/ascii_ctype.h"
 #include "third_party/glog/logging.h"
 
+void ascii() {
+  for (int ch = 'A'; ch <= 'Z'; ++ch) {
+    std::cout << ascii_tolower(ch);
+  }
+  std::cout << std::endl;
+  for (int ch = 'a'; ch <= 'z'; ++ch) {
+    std::cout << ascii_toupper(ch);
+  }
+  std::cout << std::endl;
+}
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -10,5 +21,6 @@ int main(int argc, char** argv) {
   string greeting = StringPrintf("%s %s", "hello", "world");
   std::cout << greeting << std::endl;
   std::cout << atoi64("1234567") << std::endl;
+  ascii();
   return 0;
 }
